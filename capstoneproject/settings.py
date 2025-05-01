25399b9cfa04a1a0a6d648d7b30db7dbf3bd4f9c
= <=>
2025-05-01 23:13:00 +0000

    Database configuration...

--- capstoneproject/settings.py
+++ capstoneproject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "capstoneproject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "capstonedb",
+        "HOST": "127.0.0.1",
+        "USER": "root",
+        "PORT": "3306",
+        "PASSWORD": "root",
     }
 }
 
